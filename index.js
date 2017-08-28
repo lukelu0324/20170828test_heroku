20dd827d3f7ef233fcd7626bbe0bf3ff00f7022b
lukelu0324 <31204970+lukelu0324@users.noreply.github.com>
2017-08-28 15:54:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: '1532456627',
+  channelSecret: '95f6e1182e9dfc44326563ccd1a960f0',
+  channelAccessToken: 'XMmH173RRphLqCr7JGYqLnw9tKCTb0zB1+S6Bl0KX1GyKwIP5Uk1tEXPHahBt4CrA3SJk3w3ycgy7vB4Xb2+F/cibAAfPAdtWUajGdHQcQ/lfK/0KQ6tim2+toriogZ1hGxdfTbquKGGlj8aiE7vNAdB04t89/1O/w1cDnyilFU='
+});
+bot.on('message', function(event) {
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
